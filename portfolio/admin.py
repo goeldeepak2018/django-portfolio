21a6fc34ce6bcee87ca046e5cdb8bcb6e835c589
Deepak Goel <goeldeepak2018@gmail.com>
2026-09-03 12:49:32 +0530

    Build portfolio profile and site settings

---
+++ portfolio/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "headline",
+        "email",
+        "location",
+    )
+
+    search_fields = (
+        "name",
+        "headline",
+        "email",
+        "summary",
+    )
---
+++ portfolio/models.py
@@ -0,0 +1,15 @@
+class Profile(models.Model):
+    name = models.CharField(max_length=150)
+    headline = models.CharField(max_length=255)
+    summary = models.TextField()
+    profile_image = models.ImageField(
+        upload_to="profile/",
+        blank=True,
+        null=True,
+    )
+    location = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.name
